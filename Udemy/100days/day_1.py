d039d29d31f9cf7b0f21eeaaea8b7102d45d3568
szaboptr <peter.szabo@dialoguecreatives.com>
2024-09-05 19:14:09 +0200

    organise + Udemy

---
+++ Udemy/100days/day_1.py
@@ -0,0 +1,10 @@
+# print("Hello " + input("What is your name?  ") + "!")
+
+# name = input("What is your name?  ")
+# length = len(name)
+# print(length)
+
+print("Greeting! Lets generate a name..")
+city = input("Where did you grow up?\n")
+pet  = input("What is yout Pet's name?\n")
+print(f"The generated name is {city} {pet}")
