2aa78badd12effa02c24d87828bc965fe89e8338
szaboptr <peter.szabo@dialoguecreatives.com>
2024-10-12 14:56:04 +0200

    UPDATE-> fix unittest 67

--- CUBIX/home_work/home_work_67_73_79/67/quiz/quiz_unittest.py
+++ CUBIX/home_work/home_work_67_73_79/67/quiz/quiz_unittest.py
@@ -1,25 +1,25 @@
 import unittest
 from quiz import check_answer
 from unittest.mock import patch
-from quiz_util import questions, answers
+from quiz_util import questions_and_answers
 
 class TestCheckAnswer(unittest.TestCase):
     
-    @patch('builtins.input', side_effect=[4, 22, 1.5, 1, 8])
+    @patch('builtins.input', side_effect=[4, 22, 1.5, 1, 8, 'p'])
     def test_check_answer_all_correct(self, mock_input):
         score = 0
         expected_score = 5
         result = check_answer(score)
         self.assertEqual(result, expected_score)
 
-    @patch('builtins.input', side_effect=[4, 22, 1.5, 0, 0])
+    @patch('builtins.input', side_effect=[4, 22, 1.5, 0, 0, 'p'])
     def test_check_answer_partial_correct(self, mock_input):
         score = 0
         expected_score = 3
         result = check_answer(score)
         self.assertEqual(result, expected_score)
 
-    @patch('builtins.input', side_effect=[0, 0, 0, 0, 0])
+    @patch('builtins.input', side_effect=[0, 0, 0, 0, 0, 'q'])
     def test_check_answer_all_wrong(self, mock_input):
         score = 0
         expected_score = 0
